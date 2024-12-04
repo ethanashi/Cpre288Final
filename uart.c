ef0f85da22e31377a64e11f6f9e410bf76cef701
jkotzamanis <111583386+jkotzamanis@users.noreply.github.com>
2024-12-03 18:21:08 -0600

    added parts to uart handler uart.c

--- uart.c
+++ uart.c
@@ -108,8 +108,68 @@ void UART1_Handler(void) {
     if (UART1_MIS_R & UART_MIS_RXMIS) {
         flag = 1;
         // Read the received data
-        uart_data = UART1_DR_R & 0xFF;
+        //uart_data = UART1_DR_R & 0xFF; //THIS WAS COMMENTED OUT URING FILE CHANGES. 
 
+        //CHANGES TO FILE START HERE 
+        char my_data;       // Variable to get bytes from Client
+        char command[100];  // Buffer to store command from Client
+        int index = 0;      // Index position within the command buffer
+
+        // Write to LCD so that we know the program is running
+        lcd_printf("Running");
+  
+        volatile boolean not_quit = true; 
+
+        while(not_quit)
+        {
+    
+            index = 0;  // Set index to the beginning of the command buffer
+            my_data = uart_receive(); // Get first byte of the command from the Client
+    
+            // Get the rest of the command until a newline byte (i.e., '\n') received
+            while(my_data != '\n' )
+            {
+            command[index] = my_data;  // Place byte into the command buffer
+            index++;
+            my_data = uart_receive(); // Get the next byte of the command
+            }
+    
+            command[index] = '\n';  // place newline into command in case one wants to echo the full command back to the Client
+            command[index+1] = 0;   // End command C-string with a NULL byte so that functions like printf know when to stop printing
+    
+            lcd_printf("Got: %s", command);  // Print received command to the LCD screen
+    
+            // Send a response to the Client (Starter Client expects the response to end with \n)
+            // In this case I am just sending back the first byte of the command received and a '\n'
+            uart_sendChar(command[0]);
+    
+            // Only send a '\n' if the first byte of the command is not a '\n',
+            // to avoid sending back-to-back '\n' to the client
+            if(command[0] != '\n')
+            {
+            uart_sendChar('\n');
+            }
+    
+    
+            switch (command[index-1]){
+                case 'M': scan(); 
+                    break; 
+                case 'R': turn_clockwise(oi_t *sensor, 20); 
+                    break; 
+                case 'L': turn_counter(oi_t *sensor, 20); 
+                    break; 
+                case 'F': move_forward(oi_t *sensor, 100);  
+                    break; 
+                case 'B': move_backward(oi_t *sensor, 100); 
+                   break; 
+                case 'A': not_quit = false; 
+                    break; 
+                default: ; //do nothing. 
+            }
+        }
+
+
+        //CHANGES TO FILE END HERE 
         // (Optional) Handle any errors (e.g., overrun, framing errors)
 
         // Clear the interrupt flag by writing to the interrupt clear register
